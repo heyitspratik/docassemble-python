f4ee3594ba8a4d0f4b05d58d853d6d1350fd2529
ER <pratikahir001@gmail.com>
2022-08-23 15:59:35 +0530

    module for document generation

--- docassemble/currency/views.py
+++ docassemble/currency/views.py
@@ -4,6 +4,11 @@ from .models import QuestionData
 from .forms import QuestionForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from textwrap import wrap
 
 
 class firstquestionview(View):
@@ -67,4 +72,20 @@ class thirdquestionview(View):
 
 class successview(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "html/success.html")
+        que_obj = QuestionData.objects.all().order_by('-id')
+        buffer = io.BytesIO()
+        p = canvas.Canvas(buffer,pagesize=A4)
+        t = p.beginText()
+        t.setFont('Helvetica-Bold', 10)
+        t.setCharSpace(2)
+        t.setTextOrigin(30, 800)
+        text = "(1) What is your total income per year?\n" + "-" + str(que_obj[2].value) + "   (2) How much is your house worth?\n" + "-" + str(que_obj[1].value) + "   (3) How much money you are investing per year?\n" + "-" + str(que_obj[0].value)
+        wraped_text = "\n".join(wrap(text, 80)) # 80 is line width
+        t.textLines(wraped_text)
+
+        p.drawText(t)
+        p.showPage()
+        p.save()
+
+        buffer.seek(0)
+        return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
